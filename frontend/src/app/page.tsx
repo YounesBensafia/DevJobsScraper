4162a39efbb0c82f8ce4bed808b4de9150c90ba4
YounesBensafia <younes.bensafia@etu.usthb.dz>
2025-06-27 18:41:36 +0100

    fix: update commented header styling for better visibility in Home component

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -4,7 +4,7 @@ import Filter from "@/components/Filter";
 export default function Home() {
   return (
     <main className="p-6">
-      {/* <h1 className="text-3xl font-bold mb-6 flex justify-center">Offres d’emploi Dev</h1> */}
+      {/* <h1 className="text-5xl font-bold mb-6 flex justify-center text-black">Remote jobs offers in Dev</h1> */}
       {/* <Filter /> */}
       <JobList />
     </main>
